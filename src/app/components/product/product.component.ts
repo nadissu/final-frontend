ca4bd8f0c58e062d9325fa262999348bfea7576c
nadid <nadid@DESKTOP-I95TG51>
2021-03-12 00:49:36 +0300

    Response.Data

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  products:Product[]=[];
+  dataloaded=false;
+  
+  constructor(private productService:ProductService) { }
+
+  ngOnInit(): void {
+    this.getProducts();   
+  }
+  getProducts(){
+  
+    this.productService.getProducts().subscribe
+    (response=>
+      {this.products=response.data
+        this.dataloaded=true;
+      })
+     
+  }
+}
