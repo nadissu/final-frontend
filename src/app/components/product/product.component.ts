e193d1a073e5262702d6674c4ba5bfc9c4b72b8c
nadid <nadid@DESKTOP-I95TG51>
2021-03-14 17:38:08 +0300

    CategoryServices and filter

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
+
 import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-product',
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,18 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Category } from '../models/category';
 import { ListResponseModel } from '../models/ListResponseModel';
-import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
-  apiUrl="https://localhost:44320/api/products/getall";
+export class CategoryService {
+  apiUrl="https://localhost:44320/api/categories/getall";
   constructor(private httpClient:HttpClient) { }
 
-  getProducts():Observable<ListResponseModel<Product>>{
-    return this.httpClient.get<ListResponseModel<Product>>
+  getCategories():Observable<ListResponseModel<Category>>{
+    return this.httpClient.get<ListResponseModel<Category>>
     (this.apiUrl);
   }
 }
